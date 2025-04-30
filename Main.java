1f301ac76cb2f52e6a3f3c4b60fa8fc3657d335f
Mart21 <61964744+Mart21@users.noreply.github.com>
2025-04-30 14:16:50 +0000

    odd or even in java

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	   Scanner myScanner = new Scanner(System.in);
+	   
+	    System.out.print("Enter a number: ");
+	    int input = myScanner.nextInt();
+	      
+	    if(input % 2==0){
+	        System.out.print("You entered even number!");
+	    }
+	    else
+	       System.out.print("You entered odd number!");
+	       myScanner.close();
+	    
+	}
+
+}
