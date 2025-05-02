7322e7a5eebf029e59c5f7262b57f2795016fc22
Mart21 <61964744+Mart21@users.noreply.github.com>
2025-05-02 12:30:59 +0000

    task2.java

---
+++ task2.java
@@ -0,0 +1,16 @@
+public class task2 {
+    
+    public static void main (String[] args){
+        int myNum = 3;  
+        byte myByte = 11;    
+        short myShort = 0;       
+        float myFloat = 2.0F;
+        char myLetter = 'H';         
+        boolean myBool = true;       
+        String myText = "wOrld";    
+        
+        System.out.println(myLetter+""+myNum+""+myByte+myShort+" "+myText+" "+myFloat+" "+myBool);
+
+    }
+    
+}
