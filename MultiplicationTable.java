14c3890f90155051fcbaa9af2b97486981be8ccd
Mart21 <61964744+Mart21@users.noreply.github.com>
2025-06-17 09:29:42 +0000

    multiplication

---
+++ MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int num = 10;
+        int increment = 1;
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println(increment + " x " + i + " = " + (increment * i));
+        }
+    }
+}
+
