3645ff75c8eed75726b1de1c32c4c1d0cd785882
Mart21 <61964744+Mart21@users.noreply.github.com>
2025-06-18 09:35:16 +0000

    String Methods

--- StringMethods.java
+++ StringMethods.java
@@ -1,26 +1,37 @@
 import java.util.Scanner;
+
 public class StringMethods {
-public static void main(String[] args) {
-    
-String input="";
-Scanner myScanner= new Scanner(System.in);
-System.out.print("Please enter a word: ");
-input = myScanner.nextLine();
-int L=input.length();
-System.out.println("Word size is :" +L);
+    public static void main(String[] args) {
+        String input = "";
+        Scanner myScanner = new Scanner(System.in);
 
-System.out.println(input.toUpperCase());
+        System.out.print("Please enter a word: ");
+        input = myScanner.nextLine();
 
-System.out.println(input.toLowerCase());
+        int L = input.length();
+        System.out.println("Word size is: " + L);
 
-System.out.println(input.charAt(L-1));
+        System.out.println("Words to Uppercase: " + input.toUpperCase());
+        System.out.println("Words to Lowercase: " + input.toLowerCase());
 
+        // Show last letter
+        if (L > 0) {
+            System.out.println("The last letter is: " + input.charAt(L - 1));
+        } else {
+            System.out.println("No last letter (empty string).");
+        }
 
-for(int i=1; i<6;i++){
-    System.out.print(input.charAt(i));
+        // Show letters 2 to 5 (index 1 to 5)
+        if (L >= 6) {
+            System.out.print("Second to the fifth letters are: ");
+            for (int i = 1; i < 6; i++) {
+                System.out.print(input.charAt(i));
+            }
+            System.out.println();
+        } else {
+            System.out.println("The word is too short to get letters 2 to 5.");
+        }
+
+        myScanner.close();
+    }
 }
-}
-
-}
-
-
